ca998a4f68da8c582a9479630373103417f29090
Michael Peters <michaelrespond99@gmail.com>
2022-04-04 17:48:55 -0430

    avance

--- js/app.js
+++ js/app.js
@@ -1,31 +1,52 @@
 
-
 //Establecemos las varibles de nuestro proyecto
-const generalbtn = document.getElementsById("inicio")
-const deportesbtn = document.getElementsById("deportes")
-const economiabtn = document.getElementsById("economia")
-const tecnologiabtn = document.getElementsById("tecnologia")
-const espectaculobtn = document.getElementsById("espectaculo")
-const busquedabtn = document.getElementsById("buscar")
+const generalbtn = document.getElementById("inicio");
+const sportsBtn = document.getElementById("deportes");
+const businessBtn = document.getElementById("economia");
+const technologyBtn  = document.getElementById("tecnologia");
+const entertainmentBtn = document.getElementById("espectaculo");
+const searchBtn = document.getElementById("buscar");
+const newsQuery = document.getElementById("newsQuery")
+const newsType = document.getElementById("newsType")
+const newsDetails = document.getElementById("newsdetails")
+
+let newDataArr= []; /*Guardaremos los datos recolectados en este arreglo*/
+
 
 //Apis de nuestro proyecto para obtener informacion
+const API_KEY = "79d30bd83c1347bd90ca5fd369393324"
+const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=ve&apiKey=79d30bd83c1347bd90ca5fd369393324";
+const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=Ve&category=general&apiKey=79d30bd83c1347bd90ca5fd369393324";
+const BUSSINESS__NEWS = "https://newsapi.org/v2/top-headlines?country=Ve&category=business&apiKey=79d30bd83c1347bd90ca5fd369393324";
+const SPORTS__NEWS = "https://newsapi.org/v2/top-headlines?country=Ve&category=sports&apiKey=79d30bd83c1347bd90ca5fd369393324";
+const TECHNOLOGY__NEWS = "https://newsapi.org/v2/top-headlines?country=Ve&category=technologys&apiKey=79d30bd83c1347bd90ca5fd369393324";
+const ENTERTAINMENT__NEWS = "https://newsapi.org/v2/top-headlines?country=Ve&category=entertainment&apiKey=79d30bd83c1347bd90ca5fd369393324";
+
+
+
 
 generalbtn.addEventListener("click", function(){
 
+    fetchGeneralNews();
+
 })
 
-deportesbtn.addEventListener("click", function(){
+sportsBtn.addEventListener("click", function(){
     
 })
 
-economiabtn.addEventListener("click", function(){
+businessBtn.addEventListener("click", function(){
     
 })
 
-tecnologiabtn.addEventListener("click", function(){
+technologyBtn.addEventListener("click", function(){
     
 })
 
-espectaculobtn.addEventListener("click", function(){
+entertainmentBtn.addEventListener("click", function(){
+    
+})
+
+searchBtn.addEventListener("click", function(){
     
 })
